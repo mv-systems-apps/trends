c4ad9dc36cad191ed1c153ef0f457cb6f3b07a32
mv-systems-apps <m.j.vandervelden@gmail.com>
2026-07-29 14:48:21 +0200

    Update sw.js
    Update

--- sw.js
+++ sw.js
@@ -1,1 +1,46 @@
+const CACHE_VERSION = 'trends-v10';
+const PRECACHE_URLS = [
+  './',
+  './index.html',
+  './trends.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(PRECACHE_URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Stale-while-revalidate: serveer direct uit cache, ververs op de achtergrond.
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.open(CACHE_VERSION).then((cache) =>
+      cache.match(event.request).then((cachedResponse) => {
+        const fetchPromise = fetch(event.request)
+          .then((networkResponse) => {
+            if (networkResponse && networkResponse.status === 200) {
+              cache.put(event.request, networkResponse.clone());
+            }
+            return networkResponse;
+          })
+          .catch(() => cachedResponse);
+        return cachedResponse || fetchPromise;
+      })
+    )
+  );
+});
 
